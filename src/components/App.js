1ba4cfd6cd40f9c301d3fe0f9cf1911aa5109d70
Nijhan <najmashaaban6@gmail.com>
2025-07-28 00:29:31 +0300

    react-hooks-fetch-crud-lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import AdminNavBar from "./AdminNavBar";
 import QuestionForm from "./QuestionForm";
 import QuestionList from "./QuestionList";
@@ -6,10 +6,35 @@ import QuestionList from "./QuestionList";
 function App() {
   const [page, setPage] = useState("List");
 
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")
+      .then((r) => r.json())
+      .then(setQuestions);
+  }, []);
+
+  function handleAddQuestion(newQuestion) {
+    setQuestions((questions) => [...questions, newQuestion]);
+  }
+
+  function handleDeleteQuestion(id) {
+    fetch(`http://localhost:4000/questions/${id}`, { method: "DELETE" })
+      .then((r) => {
+        if (r.ok) {
+          setQuestions((questions) => questions.filter((q) => q.id !== id));
+        }
+      });
+  }
+
   return (
     <main>
       <AdminNavBar onChangePage={setPage} />
-      {page === "Form" ? <QuestionForm /> : <QuestionList />}
+      {page === "Form" ? (
+        <QuestionForm onAddQuestion={handleAddQuestion} />
+      ) : (
+        <QuestionList questions={questions} onDelete={handleDeleteQuestion} />
+      )}
     </main>
   );
 }
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import QuestionItem from "./QuestionItem";
+
 
 function QuestionList() {
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")
+      .then((r) => r.json())
+      .then(setQuestions);
+  }, []);
+
+  function handleDelete(id) {
+    fetch(`http://localhost:4000/questions/${id}`, { method: "DELETE" })
+      .then((r) => {
+        if (r.ok) {
+          setQuestions((questions) => questions.filter((q) => q.id !== id));
+        }
+      });
+  }
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>
+        {questions.map((q) => (
+          <QuestionItem key={q.id} question={q} onDelete={handleDelete} />
+        ))}
+      </ul>
     </section>
   );
 }
